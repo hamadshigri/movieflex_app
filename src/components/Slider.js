a8258699a8099e532e51431fbaf09a5dda26be4e
hamadshigri <hamad.shigri95@gmail.com>
2022-11-18 12:15:13 +0500

    Worked on Header and Slider

---
+++ src/components/Slider.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Slideimage1 from "./images/money-heist.webp";
+import Slideimage2 from "./images/comic-book.jpg";
+import Slideimage3 from "./images/black-adam.jpg";
+
+const Slider = () => {
+  return (
+    <div>
+      <div
+        id="carouselExampleIndicators"
+        class="carousel slide"
+        data-bs-ride="true"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={Slideimage1} class="d-block w-100" alt="" />
+          </div>
+          <div class="carousel-item">
+            <img src={Slideimage2} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={Slideimage3} class="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
